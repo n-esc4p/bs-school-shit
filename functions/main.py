54690bf75ef63889e39e47d2d48d6f72d5fdf466
n-esc4p <edvardradesey@gmail.com>
2022-02-16 10:50:01 +0100

    added methods and functions

---
+++ functions/main.py
@@ -0,0 +1,13 @@
+def tbl7():
+    for n in range(11):
+        print(n*7)
+
+
+def tbl8():
+    for n in range(11):
+        print(n*8)
+
+
+tbl7()
+print("\n")
+tbl8()
---
+++ methods/main.py
@@ -0,0 +1,8 @@
+nl = [2, 8, 1, 6, 0]
+print(nl)
+nl.sort()
+print(nl)
+nl.reverse()
+print(nl)
+nl.remove(6)
+print(nl)
